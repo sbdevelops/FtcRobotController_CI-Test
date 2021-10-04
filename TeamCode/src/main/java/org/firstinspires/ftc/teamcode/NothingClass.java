e0f07923e18c9f42e62eb24269df24e7cb844049
Steven Baker <42903115+sbdevelops@users.noreply.github.com>
2021-10-04 16:12:08 -0400

    Update NothingClass.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NothingClass.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NothingClass.java
@@ -1,5 +1,5 @@
 package org.firstinspires.ftc.teamcode;
-// A class that does absolutely nothing
+// A class that does absolutely nothing, not very useful for a competition.
 public class NothingClass {
   public static void doNothing() { }
 }
